4cdf012dfde053d829c735927a452894dfb563ea
Vilho Teivainen <vilho.teivainen@gmail.com>
2020-09-21 17:20:24 +0300

    Added the last methods, whatever they are.

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const app = express().use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // First you need to create a connection to the database
 // Be sure to replace 'user' and 'password' with the correct values
 const con = mysql.createConnection({
